32bf0e537378c3e90afef0611889b099edec363e
ram <sandhyasharma0423@gmail.com>
2017-10-26 02:49:26 +0530

    Loading resoucres

---
+++ myshop/src/main/java/com/sandy/myshop/controller/PageController.java
@@ -0,0 +1,20 @@
+package com.sandy.myshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController
+{
+	@RequestMapping(value={"/","/home"})
+	public ModelAndView index()
+	{
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("msg","hello spring");
+		
+		return mv;
+	}
+	
+
+}
