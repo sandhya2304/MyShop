368c9d531e685aa17898d9e97aaa9aa7dd1ff455
ram <sandhyasharma0423@gmail.com>
2017-10-26 19:41:02 +0530

    complete till theme

--- myshop/src/main/java/com/sandy/myshop/controller/PageController.java
+++ myshop/src/main/java/com/sandy/myshop/controller/PageController.java
@@ -7,14 +7,36 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PageController
 {
-	@RequestMapping(value={"/","/home"})
+	@RequestMapping(value={"/","/home","/index"})
 	public ModelAndView index()
 	{
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("msg","hello spring");
+		mv.addObject("title","home");
+		mv.addObject("userClickHome",true);
 		
 		return mv;
 	}
 	
+	@RequestMapping(value="/about")
+	public ModelAndView about()
+	{
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout",true);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/contact")
+	public ModelAndView contactUs()
+	{
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact",true);
+		
+		return mv;
+	}
+	
+
 
 }
